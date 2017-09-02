1a0e9afcae332248d67f53b9b8447f008873ca38
branch <522839943@qq.com>
2017-09-02 10:26:00 +0800

    Update STL使用.cpp

--- STL使用.cpp
+++ STL使用.cpp
@@ -1,11 +1,11 @@
-//2
 #include <iostream>
 #include <vector>
-#include<string>
-#include<map>
-#include<xfunctional>
-#include<algorithm>
-#include<deque>
+#include <string>
+#include <map>
+#include <xfunctional>
+#include <algorithm>
+#include <deque>
+#include <functional>
 using namespace std;
 
 enum Color
@@ -47,14 +47,6 @@ bool compare(int a, int b) {
 	return  a < b;
 }
 
-//template<class T>
-//void print(vector<T> a) {
-//	cout << endl;
-//	for (vector<T>::iterator it = a.begin(); it != a.end(); it++) {
-//		cout << *it << " ";
-//	}
-//	cout << endl;
-//}
 
 int main() {
 	cout << "Map----------------------------------" << endl;
@@ -63,24 +55,15 @@ int main() {
 	ms.insert(pair<int, string>(2, "bb"));
 	ms.insert(pair<int, string>(3, "cc"));
 	ms.insert(pair<int, string>(3, "dd"));
+	ms.insert(pair<int, string>(-1, "dd1"));
 	for (map<int, string, greater<int>>::iterator it = ms.begin(); it != ms.end(); it++) {
 		cout << (*it).first << ", " << (*it).second << endl;
 	}
-	//cout << "Map after remove:" << endl;
-	//remove(ms.begin(), ms.end(), 1);
-	//for (map<int, string, greater<int>>::iterator it = ms.begin(); it != ms.end(); it++) {
-	//	cout << (*it).first << ", " << (*it).second << endl;
-	//}
 	cout << "Map after erase:" << endl;
 	ms.erase(2);
 	for (map<int, string, greater<int>>::iterator it = ms.begin(); it != ms.end(); it++) {
 		cout << (*it).first << ", " << (*it).second << endl;
 	}
-	//cout << "Map after reverse:" << endl;
-	//reverse(ms.begin(), ms.end());
-	//for (map<int, string, greater<int>>::iterator it = ms.begin(); it != ms.end(); it++) {
-	//	cout << (*it).first << ", " << (*it).second << endl;
-	//}
 
 	cout << endl<<"Vector----------------------------------" << endl;
 	vector<int> ary = { 2,3,4,1,4,0,6,5 };
@@ -113,8 +96,27 @@ int main() {
 	it_de1 = find(de.begin(), de.end(), 2);
 	it_de2 = find(de.begin(), de.end(), 2);
 	//for_each(it_de1, it_de2, print<int>);
-
 	deque<int>::reverse_iterator rit_de;
+
+	cout << endl << "函数对象----------------------------------" << endl;
+	minus<int> int_minus;
+	cout << int_minus(3, 4) << endl;
+
+	cout << endl << "函数适配器bind1st,bind2nd----------------------------------" << endl;
+	binder1st<minus<int>> minusob = bind1st(minus<int>(), 10);
+	cout <<"binder1st minusob(20): " << minusob(20) << endl;
+	binder2nd<minus<int>> minusob2 = bind2nd(minus<int>(), 10);
+	cout << "binder2nd minusob(20): " << minusob(20) << endl;
+
+	vector<int> vec = { 0,1,2,3,4,5,6,7,8,9 };
+	//返回数组中大于等于 或 小于等于 n的数: count_if(vec.begin(), vec.end(), bind1st(less_equal<int>(), n));
+	int bigger = count_if(vec.begin(), vec.end(), bind1st(less_equal<int>(), 3));//3<=bigger
+	int smaller = count_if(vec.begin(), vec.end(), bind1st(greater_equal<int>(), 3));//3>=smaller
+	cout << bigger << " " << smaller << endl;
+	int bigger2 = count_if(vec.begin(), vec.end(), bind2nd(greater_equal<int>(), 3));//bigger2>=3
+	int smaller2 = count_if(vec.begin(), vec.end(), bind2nd(less_equal<int>(), 3));//smaller2<=3
+	cout << bigger2 << " " << smaller << endl;
+
 
 	system("pause");
 	return 0;
